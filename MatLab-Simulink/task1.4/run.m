af16bf878b92dde25eacf56223a479a44a1401f1
erikliland <erik.liland@gmail.com>
2015-11-04 15:28:50 +0100

    Added nonlinear models in 1.4

--- MatLab-Simulink/task1.4/run.m
+++ MatLab-Simulink/task1.4/run.m
@@ -64,7 +64,31 @@ saveas(fig2,'Task1_4_Nomoto2.eps','epsc');
 
 %% 
 
-% Nomoto 1. ordens ulineær model
-
-
 % Nomoto 2. ordens ulineær model (Norbin?)
+fig3 = figure('OuterPosition',[scrsz(3)*2/3 0 scrsz(3)/3 scrsz(4)/2]);
+hold on; grid off; ylabel('Yaw rate [deg/s]'); xlabel('Time [s]');
+title('2st order non-linear Nomoto model compared to ship response','FontSize',14);
+
+for delta = 5:10:25
+    delta_c = deg2rad(delta); %maks +-25deg
+    sim MSFartoystyring;
+    x0 = [2000 100 3000 50]';
+    F_3 = @(x,t,delta_c) delta_c*(x(4) - (x(4)*exp(-t/x(1))*(x(1) - x(3)))/(x(1) - x(2)) + (x(4)*exp(-t/x(2))*(x(2) - x(3)))/(x(1) - x(2)));
+    F_4 = @(x,t) F_3(x,t,delta_c);
+    x = lsqcurvefit(F_4, x0, t, r,[],[],OPT);
+    T1 = x(1);
+    T2 = x(2);
+    T3 = x(3);
+    K1 = x(4);
+    nomoto2 = delta_c * (K1 - (K1*exp(-t/T1)*(T1 - T3))/(T1 - T2) + (K1*exp(-t/T2)*(T2 - T3))/(T1 - T2));
+    plot(t, rad2deg(r),'--' )
+    plot(t, rad2deg(nomoto2));
+end
+axis([0 tstop 0 0.55]);
+legend( 'Ship, \delta = 5' ,'Non-lin Nomoto2, \delta = 5', ...
+        'Ship, \delta = 15','Non-lin Nomoto2, \delta = 15',...
+        'Ship, \delta = 25','Non-lin Nomoto2, \delta = 25'); 
+saveas(fig3,'Task1_4_Nomoto2_nonlin.eps','epsc');
+
+
+% Nomoto 1. ordens ulineær model
