0e5c1e91d15da7899c61c0de0e167b73086b6bbf
erikliland <erik.liland@gmail.com>
2015-11-09 10:19:33 +0100

    Lagt inn regulator fra 1.4 og 1.8. Feilmargin må legges inn i 2.3

--- MatLab-Simulink/task1.8/run_task1_8.m
+++ MatLab-Simulink/task1.8/run_task1_8.m
@@ -1,38 +1,4 @@
 close all; clear all; clc; scrsz = get(groot,'ScreenSize'); load('../task1.4/H_b_curvefitting.mat');
-tstart= 0;                  %Sim start time
-tstop = 5000;               %Sim stop time
-tsamp = 10;                  %Sampling time (NOT ODE solver time step)
-
-%System
-p0  = zeros(2,1);           %Initial position (NED)
-v0  = [0.001 0]';           %Initial velocity (body)[m/s]
-psi0= 0;                    %Inital yaw angle [rad]
-r0  = 0;                    %Inital yaw rate [rad]
-c   = 0;                    %Current on (1)/off (0)
-
-%Yaw rate controller
-delta_max=deg2rad(25);      %Ships rudder maximum angle [rad]
-delta_dot_max=deg2rad(0.4); %Ships rudder slew rate     [rad/s]
-r_max=deg2rad(0.35);        %Ships maximum yaw rate (delta_c = max) [rad]
-w_r = deg2rad(0.3);         %r_d 1st order LPF, w_r = -3dB cutoff 
-r_d = deg2rad(0.1);         %Desired yaw rate           [rad/s]
-k_b0 = 0.009;               %Offset compansation            [rad]
-k_b3 = b_2(1);              %Cubic feed forward             [rad/(rad/s)]
-k_b1 = b_2(2);              %Linear feed forward            [rad/(rad/s)]
-k_b2 = b_2(3);              %Quadratic feed forward         [rad/(rad/s)]
-kp_r = 300;                 %Feedback proportional error gain
-ki_r = 1/3;                 %Feedback integral error gain
-kd_r = 5e3;                 %Feedback derivative error gain
-N_r  = deg2rad(0.8);         %Derivative filter [LPF] cutoff frequency [rad/s]
- 
-%Yaw / Heading controller
-w_psi  = deg2rad(0.2);      %psi_d 1st order LPF, w_psi = -3dB cutoff 
-psi_s  = deg2rad(45);       %Desired heading [rad]
-psi_t  = 2000;              %Step time
-kp_psi = 15e-3;             %Feedback proportional error gain
-kd_psi = 3;                 %Feedback derivative error gain
-ki_psi = 2e-5;              %Feedback integral error gain
-N_psi  = deg2rad(1);        %Derivative filter [LPF] cutoff frequency [rad/s]
 
 %Speed controller
 n_c_max = 8.9;              %Propeller shaft max velocity [rad/s](85 rpm)
@@ -45,6 +11,21 @@ K_p_u = 5;                  %Feedback proportional error gain
 K_i_u = 1e-3;               %Feedback integral error gain
 K_d_u = 0;                  %Feedback derivative error gain
 N_u = deg2rad(1);           %Derivative filter [LPF] cutoff frequency [rad/s]
+save('Speed controller')
+
+%Simulation
+tstart= 0;                  %Sim start time
+tstop = 5000;               %Sim stop time
+tsamp = 10;                  %Sampling time (NOT ODE solver time step)
+
+%System
+p0  = zeros(2,1);           %Initial position (NED)
+v0  = [0.001 0]';           %Initial velocity (body)[m/s]
+psi0= 0;                    %Inital yaw angle [rad]
+r0  = 0;                    %Inital yaw rate [rad]
+c   = 0;                    %Current on (1)/off (0)
+
+load('../task1.4/Yaw and Yaw-rate controller');
 
 sim task1_8
 
