307cae1da1a9c2bac4b8ca25114a33d6bdedd1a8
erikliland <erik.liland@gmail.com>
2015-11-09 11:32:23 +0100

    Lagt til og tunet PID i 1.4

--- MatLab-Simulink/task1.4/run_task1_4.m
+++ MatLab-Simulink/task1.4/run_task1_4.m
@@ -23,22 +23,21 @@ kp_psi = 15e-3;             %Feedback proportional error gain
 kd_psi = 3;                 %Feedback derivative error gain
 ki_psi = 2e-5;              %Feedback integral error gain
 N_psi  = deg2rad(1);        %Derivative filter [LPF] cutoff frequency [rad/s]
-save('Yaw and Yaw-rate controller')
+save('Yaw_NonLin_controller'); clear all;
 
 %Yaw / Heading controller (PID)
-w_psi  = 0.04;              %psi_d 1st order LPF, w_psi = -3dB cutoff 
-psi_s  = deg2rad(45);       %Desired heading [rad]
-psi_t  = 2000;              %Step time
-kp_psi = 15e-3;             %Feedback proportional error gain
-kd_psi = 3;                 %Feedback derivative error gain
-ki_psi = 2e-5;              %Feedback integral error gain
-N_psi  = deg2rad(1);        %Derivative filter [LPF] cutoff frequency [rad/s]
+kp_psi = 90;                %Feedback proportional error gain
+ki_psi = 0.8;               %Feedback integral error gain
+kd_psi = 500;               %Feedback derivative error gain
+save('Yaw_PID_controller'); clear all;
 
+load('Yaw_NonLin_controller');
+load('Yaw_PID_controller');
 
 %Simulink
 tstart= 0;                  %Sim start time
-tstop = 5000;               %Sim stop time
-tsamp = 10;                  %Sampling time (NOT ODE solver time step)
+tstop = 1500;               %Sim stop time
+tsamp = 1;                  %Sampling time (NOT ODE solver time step)
 
 %System
 p0  = zeros(2,1);           %Initial position (NED)
@@ -50,33 +49,34 @@ n_c = 7.3;                  %Propeller shaft speed [rad/s]
 
 %Model
 load('H_b_curvefitting');
+load('Nomoto2_curvefitting');
 
 sim task1_4
 
 scrsz = get(groot,'ScreenSize');
 fig1 = figure('OuterPosition',[0 0 scrsz(3) scrsz(4)]);
 subplot(2,2,1); hold on; xlabel('Time [s]'); ylabel('Heading error [deg]');
-plot(t,rad2deg(psi_e));
-plot(t,rad2deg(psi_e_m))
+plot(t,rad2deg(psi_e_PID));
+plot(t,rad2deg(psi_e_PID_m),'-.')
 legend('\psi_e','\psi_{e_m}','Location','NorthEast');
 title('Yaw error');
 
 subplot(2,2,2); hold on; xlabel('Time [s]'); ylabel('Heading [deg]');
 plot(t,rad2deg(psi_d),'--');
 plot(t,rad2deg(psi));
-plot(t,rad2deg(psi_m));
+plot(t,rad2deg(psi_m),'-.');
 legend('\psi_d','\psi','\psi_m','Location','NorthEast');
 title('Yaw controller');
 
 subplot(2,2,3); hold on; xlabel('Time [s]'); ylabel('Angle [deg]');
-plot(t,rad2deg(r_e));
-plot(t,rad2deg(r_e_m));
+plot(t,rad2deg(r_e_PID));
+plot(t,rad2deg(r_e_PID_m),'-.');
 legend('r_e','r_{e_m}','Location','NorthEast');
 title('Yaw rate error');
 
 subplot(2,2,4); hold on; xlabel('Time [s]'); ylabel('Yaw rate [deg/s]');
-plot(t,rad2deg(rd),'--');
+plot(t,rad2deg(r_d_PID),'--');
 plot(t,rad2deg(r));
-plot(t,rad2deg(r_m));
+plot(t,rad2deg(r_m),'-.');
 legend('r_d','r','r_m','Location','NorthEast');
 title('Yaw rate controller');
