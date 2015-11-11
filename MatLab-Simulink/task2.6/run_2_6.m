ed1c8d305762d99929c71942bec8147c176c1983
erikliland <erik.liland@gmail.com>
2015-11-11 16:48:39 +0100

    Testing av 2.6

--- MatLab-Simulink/task2.6/run_2_6.m
+++ MatLab-Simulink/task2.6/run_2_6.m
@@ -5,7 +5,7 @@ load('../task1.4/Yaw_PID_controller');
 load('../task1.8/Speed controller')
 
 tstart = 0;      %Sim start time
-tstop = 4200;    %Sim stop time
+tstop = 2600;    %Sim stop time
 tsamp = 10;      %Sampling time (NOT ODE solver time step)
 
 p0 = zeros(2,1); %Initial position (NED)
@@ -13,6 +13,11 @@ v0 = [6.63 0]';  %Initial velocity (body)
 psi0 = 0;        %Inital yaw angle
 r0 = 0;          %Inital yaw rate
 c = 0;           %Current on (1)/off (0)
+
+%%%% Design values
+R = 1200; %THE LOOKAHEAD DISTANCE (R>600m due to initial state)
+Ua_MAX = 1; % [m/s];
+DELTA_s = 0.01; % should be > 0
 
 sim task2_6
 
@@ -22,4 +27,15 @@ fig3 = figure('OuterPosition',[0 0 scrsz(3)/2 scrsz(4)/2]);
 hold on; xlabel('Time [s]'); ylabel('Angle [deg]');
 plot(t,rad2deg(psi_d),'--');
 plot(t,rad2deg(psi));
-legend('\psi_d','\psi');
+plot(t,rad2deg(chi_d),'--');
+plot(t,rad2deg(beta));
+plot(t,rad2deg(psi_d_f),'-.');
+legend('\psi_d','\psi','\chi_d','\beta','\psi_{d_f}');
+
+
+fig4 = figure('OuterPosition',[scrsz(3)/2 0 scrsz(3)/2 scrsz(4)/2]);
+hold on; xlabel('Time [s]'); ylabel('Speed [m/s]');
+plot(t,u_d,'--');
+plot(t,U_ship(:,1));
+plot(t,U_d);
+legend('u_d','u','U_d');
