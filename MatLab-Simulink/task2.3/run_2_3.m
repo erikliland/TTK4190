c38e684cf9da8ef5f6279fadb344843c9d5f8a17
erikliland <erik.liland@gmail.com>
2015-11-13 07:34:13 +0100

    Mindre endringer i 2.3

--- MatLab-Simulink/task2.3/run_2_3.m
+++ MatLab-Simulink/task2.3/run_2_3.m
@@ -4,15 +4,17 @@ load('WP.mat');
 load('../task1.4/Yaw_PID_controller');
 load('../task1.8/Speed controller')
 
+ki_psi = 0.6;
+
 tstart = 0;      %Sim start time
-tstop = 2500;    %Sim stop time
+tstop = 2600;    %Sim stop time
 tsamp = 10;      %Sampling time (NOT ODE solver time step)
 
 p0 = zeros(2,1); %Initial position (NED)
 v0 = [6.63 0]';  %Initial velocity (body)
 psi0 = 0;        %Inital yaw angle
 r0 = 0;          %Inital yaw rate
-c = 0;           %Current on (1)/off (0)
+c = 1;           %Current on (1)/off (0)
 
 %%%% Design values
 Ua_MAX = 1; % [m/s];
@@ -25,6 +27,7 @@ pathplotter(p(:,1), p(:,2),  psi, tsamp, 1, tstart, tstop, 0, WP)
 
 fig3 = figure('OuterPosition',[0 0 scrsz(3)/2 scrsz(4)/2]);
 hold on; xlabel('Time [s]'); ylabel('Angle [deg]');
-plot(t,rad2deg(psi_d),'--');
+plot(t,rad2deg(psi_d),'-.');
+plot(t,rad2deg(psi_d_f),'--');
 plot(t,rad2deg(psi));
-legend('\psi_d','\psi');
+legend('\psi_d','\psi_{d_f}','\psi');
